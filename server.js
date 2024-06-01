f173bb40cbd5eadfa6eb3b2614238da9d2e257fe
reivaJSnevet <128876020+reivaJSnevet@users.noreply.github.com>
2024-06-01 13:20:00 -0600

    chore: Update server.js to include errorHandler middleware and remove seedDatabase function call

--- server.js
+++ server.js
@@ -6,14 +6,14 @@ import corsOptions from './app/config/corsOptions.js';
 import db from './app/config/config.js';
 
 import logger from './app/middlewares/logger.js';
+import errorHandler from './app/middlewares/errorHandler.js';
+
 
 import productRoutes from './app/routes/productRoutes.js';
 import orderRoutes from './app/routes/orderRoutes.js';
 import userRoutes from './app/routes/userRoutes.js';
 import orderItemRoutes from './app/routes/orderItemRoutes.js';
 import categoryRoutes from './app/routes/categoryRoutes.js';
-
-import seedDatabase from './seed.js';
 
 dotenv.config();
 
@@ -26,9 +26,9 @@ app.use(cors(corsOptions));
 
 app.get('/', (req, res) => {
     res.json({ message: 'Welcome to the API!' });
-    });
+});
 app.use('/api', (req, res) => {
-  res.json({ message: 'API is running...' });
+    res.json({ message: 'API is running...' });
 });
 app.use('/api/products', productRoutes);
 app.use('/api/orders', orderRoutes);
@@ -36,13 +36,15 @@ app.use('/api/users', userRoutes);
 app.use('/api/orderItems', orderItemRoutes);
 app.use('/api/categories', categoryRoutes);
 
+app.use(errorHandler);
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on port ${PORT}`);
 });
 
 db.authenticate().then(() => {
     console.log('Database connected...');
-  }).catch(err => {
+}).catch(err => {
     console.log('Error: ' + err);
-  });
+});
